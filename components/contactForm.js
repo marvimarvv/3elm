84cbc0f543d9c095fccafa97066231978347dafc
marvimarvv <marvin.tandler@outlook.com>
2023-04-12 00:04:14 +0200

    Maybe now?

--- components/contactForm.js
+++ components/contactForm.js
@@ -11,6 +11,7 @@ export default function ContactForm({ className }) {
       method="POST"
       className={className}
     >
+      <input type="hidden" name="form-name" value="contact" />
       <p class="hidden">
         <label>
           Don’t fill this out if you’re human: <input name="bot-field" />
@@ -22,6 +23,7 @@ export default function ContactForm({ className }) {
           type="text"
           name="fullname"
           className="mt-1 h-9 w-full p-3 text-darkorange"
+          required
         />
       </label>
       <label>
@@ -30,6 +32,7 @@ export default function ContactForm({ className }) {
           type="email"
           name="emailaddress"
           className="mt-1 h-9 w-full p-3 text-darkorange"
+          required
         />
       </label>
       <label className="flex flex-grow flex-col">
@@ -37,6 +40,7 @@ export default function ContactForm({ className }) {
         <textarea
           name="message"
           className="mt-1 min-h-[10rem] w-full flex-grow resize-none p-3 text-darkorange"
+          required
         ></textarea>
       </label>
       <button

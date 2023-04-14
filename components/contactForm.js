848a8725e191791903c7409450731e2e5d3b782e
marvimarvv <marvin.tandler@outlook.com>
2023-04-14 20:13:27 +0200

    Check if it works w/o html file and with styling

--- components/contactForm.js
+++ components/contactForm.js
@@ -4,17 +4,54 @@ export default function ContactForm({ className }) {
   const { t } = useTranslation();
 
   return (
-    <form method="POST" name="contact-form" data-netlify="true">
+    <form
+      method="POST"
+      netlify-honeypot="bot-field"
+      name="contact-form"
+      data-netlify="true"
+      className={className}
+    >
       <input type="hidden" name="form-name" value="contact-form" />
-      <label htmlFor="name">Name *</label>
-      <input id="name" name="name" required type="text" />
-      <label htmlFor="company">Company *</label>
-      <input id="company" name="company" required type="text" />
-      <label htmlFor="email">E-mail Address *</label>
-      <input id="email" type="email" name="email" required />
-      <label htmlFor="message">Message *</label>
-      <textarea id="message" name="message" required></textarea>
-      <button type="submit">Submit</button>
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you’re human: <input name="bot-field" />
+        </label>
+      </p>
+      <div>
+        <label htmlFor="name">{t("contact.form name")}</label>
+        <input
+          id="name"
+          name="name"
+          className="mt-1 h-9 w-full p-3 text-darkorange"
+          required
+          type="text"
+        />
+      </div>
+      <div>
+        <label htmlFor="email">{t("contact.form message")}</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          className="mt-1 h-9 w-full p-3 text-darkorange"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">{t("contact.form message")}</label>
+        <textarea
+          id="message"
+          name="message"
+          className="mt-1 min-h-[10rem] w-full flex-grow resize-none p-3 text-darkorange"
+          required
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="mt-auto flex cursor-pointer items-center justify-center rounded-full bg-darkorange p-4 transition-all hover:-translate-y-2 active:translate-y-2 active:bg-brightorange"
+      >
+        {t("contact.form button")}
+      </button>
     </form>
   );
 }

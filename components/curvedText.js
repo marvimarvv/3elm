e48ca39f03ebf7e965cd528c35e6cbe062ea957d
marvimarvv <marvin.tandler@outlook.com>
2024-02-08 20:04:47 +0100

    Fix: Disable curved text on Safari bc of browser bug

--- components/curvedText.js
+++ components/curvedText.js
@@ -12,50 +12,61 @@ const calculateStartOffset = (pathId, textContent) => {
 
 const CurvedText = ({ text, className }) => {
   const [startOffset, setStartOffset] = useState(0);
+  const [isSafari, setIsSafari] = useState(false);
 
   const svgRef = useRef(null);
 
   useEffect(() => {
-    const pathId = "#curve";
-    const calculatedStartOffset = calculateStartOffset(pathId, text);
-    setStartOffset(calculatedStartOffset);
-  }, [text]);
+    // Perform the check once the component mounts to ensure window is defined
+    const userAgent = navigator.userAgent;
+    const safariCheck = /^((?!chrome|android).)*safari/i.test(userAgent);
+    setIsSafari(safariCheck);
 
-  useEffect(() => {
-    const svgContainer = svgRef.current;
-    const path = svgContainer.querySelector("#curve");
-    const pathBBox = path.getBBox();
-    const viewBox = `${pathBBox.x} ${pathBBox.y} ${pathBBox.width} ${pathBBox.height}`;
-    svgContainer.setAttribute("viewBox", viewBox);
+    if (!safariCheck) {
+      const pathId = "#curve";
+      const calculatedStartOffset = calculateStartOffset(pathId, text);
+      setStartOffset(calculatedStartOffset);
+      const svgContainer = svgRef.current;
+      const path = svgContainer.querySelector("#curve");
+      const pathBBox = path.getBBox();
+      const viewBox = `${pathBBox.x} ${pathBBox.y} ${pathBBox.width} ${pathBBox.height}`;
+      svgContainer.setAttribute("viewBox", viewBox);
+    }
   }, [text]);
 
   return (
     // Wrap this in a div to make the svg responsive, viewport units don't work on svg
     // Also, the svg otherwise always takes up 100% of the width of the container which
     // makes it have different sizes with different words in the same flex container
-    <div className={className}>
-      <svg viewBox="0 0 500 500" ref={svgRef} className="overflow-visible">
-        <path
-          id="curve"
-          d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
-          fill="transparent"
-        />
-        <text width="500" style={{ transform: "translateY(65%)" }}>
-          <textPath
-            className="fill-green"
-            xlinkHref="#curve"
-            style={{
-              textAnchor: "middle",
-              fontSize: "90px",
-              letterSpacing: "-4px",
-            }}
-            startOffset={startOffset}
-          >
-            {text}
-          </textPath>
-        </text>
-      </svg>
-    </div>
+    <>
+      {isSafari ? (
+        <span className={`text-green ${className}`}>{text}</span>
+      ) : (
+        <div className={className}>
+          <svg viewBox="0 0 500 500" ref={svgRef} className="overflow-visible">
+            <path
+              id="curve"
+              d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
+              fill="transparent"
+            />
+            <text width="500" style={{ transform: "translateY(65%)" }}>
+              <textPath
+                className="fill-green"
+                xlinkHref="#curve"
+                style={{
+                  textAnchor: "middle",
+                  fontSize: "90px",
+                  letterSpacing: "-4px",
+                }}
+                startOffset={startOffset}
+              >
+                {text}
+              </textPath>
+            </text>
+          </svg>
+        </div>
+      )}
+    </>
   );
 };
 
